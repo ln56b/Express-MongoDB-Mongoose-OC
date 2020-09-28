39ec591e73980bc521c3954d4b06827be3fc06c0
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-28 15:24:20 +0200

    corrections

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const stuffControl = require('../controllers/stuff');
+const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffControl.createThing);
-router.get('/:id', stuffControl.getAllThings);
-router.get('/', stuffControl.getOneThing);
-router.put('/:id', stuffControl.modifyThing);
-router.delete('/:id', stuffControl.deleteThing);
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
 
 module.exports = router;
